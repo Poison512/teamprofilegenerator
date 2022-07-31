388c7c1c32a56b7649217c4fb6dda114eae2d50b
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-07-30 19:22:17 -0500

    setting up html generator

---
+++ src/generatehtml.js
@@ -0,0 +1,14 @@
+//create your team
+
+function buildTeam() {
+    //make the manager
+    const createManager = Manager => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-primary text-white" >
+            <h2 class="card-title"> ${manager.getName()}</h2>
+            <h3 class="card-title">
+             `
+    }
+
+}
